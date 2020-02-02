0d8e1aacd21d64dd36f69daea46cc42f5a675989
siang <32889725+psiang@users.noreply.github.com>
2020-02-02 13:57:30 +0800

    更新lenet数据

--- __main__.py
+++ __main__.py
@@ -1,31 +1,28 @@
 from rsidea.model import *
 
-save = False
+save = True
 
-"""AlexNet demo"""
+"""lenet demo"""
 # 读取数据
 x, y = read_data.read_SIRI_WHU()
 # 分割数据
 x_train, y_train, x_test, y_test = split_data.split(x, y)
-# # 获取未训练模型
-# model = AlexNet(input_shape=x_train[0].shape, output_shape=12)
-# # 配置模型
-# model.compile(optimizer='adam',
-#               loss='sparse_categorical_crossentropy',
-#               metrics=['accuracy'])
-# 获取数据
-model = models.load_model(".\\model_data\\model\\alexnet_SIRI_WHU.h5")
-history = load_history(".\\model_data\\history\\alexnet_SIRI_WHU.json")
-# # 填入数据进行训练并保存模型
-# history = model.fit(x_train, y_train, epochs=1,
-#                     validation_data=(x_test, y_test))
-# history = history.history
-# # 模型保存
-# if save:
-#     model.save(".\\model_data\\model\\alexnet_SIRI_WHU.h5")
-#     model.save_weights(".\\model_data\\weight\\alexnet_SIRI_WHU.h5")
-#     save_history(history, ".\\model_data\\history\\alexnet_SIRI_WHU.json")
-#     print("Save done!")
+# 获取未训练模型
+model = LeNet(input_shape=x_train[0].shape, output_shape=12)
+# 配置模型
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+# 填入数据进行训练并保存模型
+history = model.fit(x_train, y_train, epochs=100,
+                    validation_data=(x_test, y_test))
+history = history.history
+# 模型保存
+if save:
+    model.save(".\\model_data\\model\\lenet_SIRI_WHU.h5")
+    model.save_weights(".\\model_data\\weight\\lenet_SIRI_WHU.h5")
+    save_history(history, ".\\model_data\\history\\lenet_SIRI_WHU.json")
+    print("Saved!")
 # 模型评测
 model.evaluate(x_test, y_test, verbose=2)
 # 画图
