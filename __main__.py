1de368bf43d7f36cff911a47ae6518d6e4cd6799
siang <32889725+psiang@users.noreply.github.com>
2020-02-06 13:33:05 +0800

    完成30轮googlenet

--- __main__.py
+++ __main__.py
@@ -7,27 +7,27 @@ from rsidea.util.history import *
 
 save = True
 
-"""resnet34 demo"""
+"""googlenet demo"""
 # 读取数据
 x, y = read_data.read_SIRI_WHU()
 # 分割数据
 
 x_train, y_train, x_test, y_test = split_data.split(x, y)
 # 获取未训练模型
-model = resnet34.ResNet34(input_shape=x_train[0].shape, output_shape=12)
+model = googlenet.GoogLeNet(input_shape=x_train[0].shape, output_shape=12)
 # 配置模型
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 # 填入数据进行训练并保存模型
-history = model.fit(x_train, y_train, epochs=50, batch_size=192,
+history = model.fit(x_train, y_train, epochs=30 , batch_size=128,
                     validation_data=(x_test, y_test))
 history = history.history
 # 模型保存
 if save:
-    model.save(".\\model_data\\model\\resnet34_SIRI_WHU.h5")
-    model.save_weights(".\\model_data\\weight\\resnet34_SIRI_WHU.h5")
-    save_history(history, ".\\model_data\\history\\resnet34_SIRI_WHU.json")
+    model.save(".\\model_data\\model\\googlenet_SIRI_WHU.h5")
+    model.save_weights(".\\model_data\\weight\\googlenet_SIRI_WHU.h5")
+    save_history(history, ".\\model_data\\history\\googlenet_SIRI_WHU.json")
     print("Saved!")
 # # 模型评测
 # models.evaluate(x_test, y_test, verbose=2)
